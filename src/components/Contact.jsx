e83a0ede5a7760931f7555b6c9aa889ab3915fee
Cesar Augusto Triana Posso <catriana0398@gmail.com>
2023-04-26 09:43:31 -0500

    feat: add what's new and contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,55 @@
-
+import emailjs from "@emailjs/browser"
+import dinner from "../assets/img/event4.jpg";
+import "./styles/contact.css";
 
 const Contact = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY
+    const SERVICE_ID = import.meta.env.VITE_SERVICE_ID
+    const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID
+
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY)
+      .then(res => console.log(res.text))
+      .catch(err => console.log(err))
+  }
+
+
   return (
-    <div>Contact</div>
+    <section className="section__contact grid--container" id="contact">
+      <form className="form" onSubmit={handleSubmit} >
+        <div className="form__content">
+          <h2 className="form__title">
+            <span>Join&nbsp;</span><span className="second__word-title">us.</span>
+          </h2>
+
+          <p className="form__description">in the sea of life, there is nothing to fear if you have God as your captain.Jain our crew!.</p>
+
+          <div className="form__div">
+            <input className="form__input" placeholder="Full Name" type="text" name="name" />
+          </div>
+          <div className="form__div">
+            <input className="form__input" placeholder="Email Address" type="text" name="email" />
+          </div>
+          <div className="form__div">
+            <input className="form__input" placeholder="Cell Phone Number" type="text" name="phone" />
+          </div>
+
+          <div className="form__div">
+            <textarea className="form__input message" placeholder="Message" name="message" id="" cols="30" rows="10"></textarea>
+          </div>
+
+          <button className="form__btn">SENT</button>
+        </div>
+        <div className="form__image-container">
+          <img className="form__image form__image--1" src={dinner} alt="" loading="lazy" />
+          <span className="label__image-form"></span>
+        </div>
+      </form>
+    </section>
   )
 }
 
--- src/components/WhatsNew.jsx
+++ src/components/WhatsNew.jsx
@@ -1,6 +1,72 @@
+import icon_events from "../assets/img/Grupo 54.svg";
+import blog1 from "../assets/img/event1.jpg";
+import blog2 from "../assets/img/event2.jpg";
+import blog3 from "../assets/img/event3.jpg";
+import blog4 from "../assets/img/event4.jpg";
+import "./styles/whatsNew.css";
+
+
 const WhatsNew = () => {
   return (
-    <div>What´sNew</div>
+    <section className="whatsNew__section" id="what's-new">
+      <div className="section__whatsNew grid--container">
+        <div className="title__whatsNew-container">
+          <img className="icon__title-whatsNew" src={icon_events} alt="" loading="lazy" />
+          <h1 className="title__whatsNew">What´s-new</h1>
+          <p className="subtitle__whatsNew">Find out about the latest news about our club</p>
+        </div>
+
+        <div className="whatsNew__images-container">
+
+          <div className="whatsNew__image">
+            <img src={blog1} alt="" loading="lazy" />
+            <div className="items__whatsNew">
+              <h2 className="whatsNew__number-first">BLOG 1</h2>
+              <p className="author__blog-first">Jane cloe</p>
+              <p className="text__whatsNew">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi et, delectus, sapiente fuga temporibus sint rerum maxime iste debitis nostrum ea illo at iusto inventore unde aspernatur.</p>
+              <span className="date__whatsNew"><span className="number__date">12 </span><br /> Aug <br /> 2016 </span>
+              <button className="whatsNew__btn">SEE MORE</button>
+            </div>
+          </div>
+
+          <div className="whatsNew__image">
+            <img src={blog2} alt="" loading="lazy" />
+            <div className="items__whatsNew">
+              <h2 className="whatsNew__number">BLOG 2</h2>
+              <p className="author__blog">Jane cloe</p>
+              <span className="date__whatsNew"><span className="number__date">12 </span><br /> Aug <br /> 2016 </span>
+            </div>
+          </div>
+
+          <div className="whatsNew__image">
+            <img src={blog3} alt="" loading="lazy" />
+            <div className="items__whatsNew">
+              <h2 className="whatsNew__number">BLOG 3</h2>
+              <p className="author__blog">Jane cloe</p>
+              <span className="date__whatsNew"><span className="number__date">12 </span><br /> Aug <br /> 2016 </span>
+            </div>
+          </div>
+
+          <div className="whatsNew__image">
+            <img src={blog4} alt="" loading="lazy" />
+            <div className="items__whatsNew">
+              <h2 className="whatsNew__number">BLOG 4</h2>
+              <p className="author__blog">Jane cloe</p>
+              <span className="date__whatsNew"><span className="number__date">12 </span><br /> Aug <br /> 2016 </span>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="whatsNew__btn-container">
+          <a className="whatsNew-button">
+            <h1 className="whatsNew-btn">MORE EVENTS</h1>
+          </a>
+        </div>
+      </div>
+
+
+    </section>
   )
 }
 
